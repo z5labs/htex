0db219a6d03a23bf00d57380cfcbc82a43459f63
zaba505 <cakub6@gmx.com>
2022-03-06 17:12:46 -0500

    feat(htex): implement cli for htex

--- htex/app/Main.hs
+++ htex/app/Main.hs
@@ -1,23 +1,37 @@
 module Main where
 
+import           Control.Monad
+
 import           HTeX.Language.Tokenizer
 
+import           Options.Applicative
+
+data Source = FilePath FilePath | StdInput
+
+readTeX :: Source -> IO String
+readTeX src = case src of
+  StdInput      -> getContents
+  FilePath path -> return path
+
+tryReadSource s = case s of
+  []  -> Nothing
+  "-" -> Just StdInput
+  _   -> Just $ FilePath s
+
+tokensCmd = command "tokens" $ info opts $ progDesc "convert TeX to lexigraphical tokens."
+  where
+    opts = toTokens <$> argument (maybeReader tryReadSource) (metavar "SRC")
+
+    toTokens :: Source -> IO ()
+    toTokens src = (putStrLn . show . tokenize "src") =<< readTeX src
+
+toCmd = command "to" $ info opts $ progDesc "convert TeX to..."
+  where
+    opts = hsubparser tokensCmd
+
+htexCmd = info (opts <**> helper) $ progDesc "htex is a tool for working with TeX, written in Haskell."
+  where
+    opts = hsubparser toCmd
+
 main :: IO ()
-main = let
-  cases = fmap (tokenize "test")
-    [ "\\word"
-    , "\\ "
-    , "\\3"
-    , "\\\\"
-    , "\\"
-    , "\\word     \n\\    \\3 \\\\"
-    , "{"
-    , "}"
-    , "{}"
-    , "{\\word}"
-    , "hello"
-    -- , "\\word hello"
-    -- , "\\321\\hello"
-    -- , "\\begin{document}"
-    ]
-  in foldMap (putStrLn . show) cases
+main = join $ execParser htexCmd
